cdd0ad3aeff5bb209b4c760f8f7e6f017a2f1509
harrz <harald.nitschinger.mig11@fh-joanneum.at>
2013-07-18 21:06:55 +0000

    Modified index.html: Added some CSS; \nPut port 8080 in web.js

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var htmlFile = "index.html";
+var fs = require('fs');
 
-var output = "";
 
-fs = require('fs');
-
-fs.readFile('index.html','utf8', function (err,data) {  if (err) {     return console.log(err); } output = data; });
+fs.readFile(htmlFile,'utf8', function (err,data) {  if (err) {     return console.log(err); } output = data; });
 
 app.get('/', function(request, response) {
-  response.send(output);
+
+    response.send(output);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
